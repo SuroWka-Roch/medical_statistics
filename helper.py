dfcbc0893f91f8875cf157d14338ea5e1cc26ef5
SuroWka-Roch <wojtek@surowka.com>
2019-11-10 17:36:15 +0100

    Połowa zadania grupowego!

--- helper.py
+++ helper.py
@@ -1,6 +1,8 @@
 import classes
 import datetime
 
+mocz = 10
+
 def drugi_akapit(ppl,time_min=datetime.date(1000,1,1), time_max=datetime.date(2200,1,1)):
     """ 
     Biorę:
@@ -58,3 +60,53 @@ def trzeci_akapit(ppl,time_min=datetime.date(1000,1,1), time_max=datetime.date(2
                     ilosc_badan_z_antybiotykami+=1
     
     return (lenght,ilosc_badan,ilosc_badan_z_antybiotykami)
+
+def grup1(ppl,time_min=datetime.date(1000,1,1), time_max=datetime.date(2200,1,1)):
+    """
+    Zwracam uwagę na datę
+    Oddaję:
+    (ile jest badań na moczu, mocz + drobnoustruj, mocz drobnoustruj + lekowrażliwość)
+    """
+
+    mocz = 0
+    mocz_drob = 0
+    mocz_drob_lek = 0
+    for p in ppl:
+        for t in p.test:
+            if  time_min<t.date and time_max >t.date:
+                if t.materialId == mocz:
+                    mocz+=1
+                    if t.Bacteria:
+                        mocz_drob+=1
+                        for b in t.Bacteria:
+                            if b.DrugSusceptibility:
+                                mocz_drob_lek+=1
+
+    return (mocz, mocz_drob, mocz_drob_lek)
+
+def grup3(ppl,time_min=datetime.date(1000,1,1), time_max=datetime.date(2200,1,1)):
+
+    sło = {}
+    for p in ppl:
+        for t in p.test:
+            if  time_min<t.date and time_max >t.date:
+                if t.materialId == mocz:
+                    for b in t.Bacteria:
+                        try:
+                            sło[b.idType] = sło[b.idType] + 1
+                        except KeyError as e:
+                            sło[b.idType] = 1
+    return sło
+
+def grup3_2(ppl, bakteriaID,time_min=datetime.date(1000,1,1), time_max=datetime.date(2200,1,1)):
+    liczba = 0
+
+    for p in ppl:
+        for t in p.test:
+            if  time_min<t.date and time_max >t.date:
+                if t.materialId == mocz:
+                    if bakteriaID in [bakteria.idType for bakteria in t.Bacteria]:
+                        liczba+=1
+    return liczba
+
+
